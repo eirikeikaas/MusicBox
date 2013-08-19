f35e518cedbd0dcb31c26ecdbb0fac4a7b887de4
Eirik Eikås <eirikeikaas@me.com>
2013-08-20 01:08:47 +0200

    Lots of changes

--- js/server.js
+++ js/server.js
@@ -7,12 +7,93 @@ var gm = require('gm');
 var fs = require('fs');
 var request = require('request');
 var Mopidy = require("mopidy").Mopidy;
+var nosql = require('nosql').load('/root/app/database.nosql');
+
+// NOSQL FUNCTIONS ////////////////////////////////
+nosql.stored.clear();
+nosql.stored.create('queueAdd', function(nosql, next, params) {
+	nosql.insert([{type: 'track', key: params.key, track: params.track, votes: params.votes, voters: [params.voter]}], function() {
+		global.queueview();
+        next();
+    });
+});
+nosql.stored.create('queueVote', function(nosql, next, params) {	
+	nosql.update(function(doc) {
+		if(doc.key == params.key && doc.type == 'track'){
+			doc.votes += 1;
+			doc.voters.push(params.voter);
+		}
+		return doc;
+	}, function() {
+		global.queueview();
+		next();
+	});	
+
+});
+nosql.stored.create('queueRemove', function(nosql, next, params) {
+nosql.remove(function(doc) {
+    	if(doc.type == 'track' && doc.key == params.key){
+	    	return doc;
+    	}
+    }, function() {
+        next();
+    });
+
+});
+nosql.stored.create('settingsSet', function(nosql, next, params) {
+	nosql.update(function(doc) {
+    	if(doc.type == 'setting' && doc.key == params.key){
+	    	doc.value = params.value;
+    	}
+        return doc;
+    }, function() {
+        next();
+    });
+});
+nosql.stored.create('settingsGet', function(nosql, next, params) {
+	next(nosql.one('doc.type == "setting" && doc.key == '+params.key));
+});
+
+// NOSQL HELPERS //////////////////////////////////
+var filterqueue = function(doc){
+    return doc;
+};
+var sortqueue = function(a, b){
+    return b.votes - a.votes;
+};
+
+// NOSQL LIVE /////////////////////////////////////
+global.queueview = function(){
+    nosql.view.create('queue', filterqueue, sortqueue);
+};
+
+// NOSQL VIEWS ///////////////////////////////////
+nosql.view.create('queue', filterqueue, sortqueue);
+
+// NOSQL SETTINGS INIT
+var defaults = [
+	{key: 'mode', value: 'single'},
+	{key: 'allow', value: 'all'},
+	{key: 'restrict', value: ''}
+];
+var settings = nosql.all(function(doc){ return doc.type == 'setting'; });
+defaults.forEach(function(o){
+	if(settings[o.key] == undefined){
+		nosql.insert({type: 'setting', key: o.key, value: o.key});
+	}
+});
+settings = null;
 
 // SERVER INIT ////////////////////////////////////
 server.listen(9090);
 
+io.set('authorization', function (handshakeData, accept) {
+	handshakeData.sessionid = handshakeData.query.sessionid;
+	accept(null, true);
+});
+
 // VARIABLES //////////////////////////////////////
-var _queue = {};
+var _queue = {}; // TODO: NoSQL
 var _current = {};
 var _auth = {'eirik': {key: "c79a08444243cae99223bee17037ee0b5b8c9d9d"}, 'filip': {key: "agnesisenga"}};
 
@@ -39,10 +120,16 @@ Object.size = function(obj){
     return size;
 };
 
+var serverlog = function(_level, _message, _aspect){
+	io.of('/system').emit('log', {level: _level, message: _message, aspect: _aspect});
+}
 var updateQueue = function(_track, _votes, _voter){
-	if(_queue[_track.uri] == undefined){
-		io.sockets.emit('log', {level: 'ok', message: 'Added '+_track.name, aspect: 'mopidy'});
-		_queue[_track.uri] = {track: _track, votes: _votes, voters: [_voter]};
+	//if(nosql.one('doc.key == "'+_track.uri+'"');
+	if(_queue[_track.uri] == undefined){ // TODO: NoSQL
+		serverlog('ok', 'Added '+_track.name, 'mopidy');
+		_queue[_track.uri] = {track: _track, votes: _votes, voters: [_voter]}; // TODO: NoSQL
+		
+		nosql.stored.execute('queueAdd', {key: _track.uri, track: _track, votes: _votes, voter: _voter});
 		
 		mopidy.tracklist.getLength().then(function(length){
 			if(length == 0){
@@ -56,11 +143,14 @@ var updateQueue = function(_track, _votes, _voter){
 		var tl = returnTracklist();
 		io.sockets.emit('list', {tracks: tl[0], votes: tl[1]});
 	}else{
-		_queue[_track.uri].voters.contains(_voter, function(found){
+		//if(nosql.one('doc.key == "'+_track.uri+'"', function(doc){});
+		_queue[_track.uri].voters.contains(_voter, function(found){  // TODO: NoSQL
 			if(!found){
-				io.sockets.emit('log', {level: 'ok', message: 'Voted '+_track.name, aspect: 'mopidy'});
-				_queue[_track.uri].votes += _votes;
-				_queue[_track.uri].voters.push(_voter); 
+				serverlog('ok', 'Voted '+_track.name, 'mopidy');
+				_queue[_track.uri].votes += _votes;  // TODO: NoSQL
+				_queue[_track.uri].voters.push(_voter);  // TODO: NoSQL
+				
+				nosql.stored.execute('queueVote', {key: _track.uri, voter: _voter});
 				
 				var tl = returnTracklist();
 				
@@ -78,7 +168,7 @@ var updateQueue = function(_track, _votes, _voter){
 	}
 	
 	mopidy.tracklist.getLength().then(function(length){
-		io.sockets.emit('log', {level: 'log', message: 'Tracklist: '+length+' Queuelist: '+size(_queue)});
+		serverlog('log', 'Tracklist: '+length+' Queuelist: '+size(_queue), 'node');  // TODO: NoSQL
 	});
 };
 var returnTracklist = function(){
@@ -86,9 +176,18 @@ var returnTracklist = function(){
 	var sortlist = [];
 	var votelist = {};
 	
-	for(key in _queue){
-		sortlist.push(_queue[key]);
-		votelist[key] = {votes: _queue[key].votes, voters: _queue[key].voters};
+	nosql.view.all('queue', function(tracks){
+		tracks.forEach(function(o){
+			votelist[o.key] = {votes: o.votes, voters: o.voters};
+		});
+		console.log("TRQ", tracks);
+		
+		return [tracks, votelist];
+	});
+	
+	for(key in _queue){  // TODO: NoSQL
+		sortlist.push(_queue[key]); // TODO: NoSQL
+		votelist[key] = {votes: _queue[key].votes, voters: _queue[key].voters}; // TODO: NoSQL
 	}
 	
 	sortlist = sortlist.slice(1);
@@ -101,7 +200,7 @@ var returnTracklist = function(){
 		trklist.push(sortlist[i].track);
 	}
 	
-	console.log(_queue);
+	//console.log(_queue); // TODO: NoSQL
 	
 	return [trklist, votelist];
 };
@@ -111,11 +210,9 @@ var download = function(uri, filename){
 	});
 };
 var working = '';
-
 var mopidy = new Mopidy({
     webSocketUrl: "ws://localhost:6680/mopidy/ws/"
 });
-
 var coverart = function(artist, album, callback){
 	console.log('Runninnnnng');
 	converted = '';
@@ -161,17 +258,17 @@ var coverart = function(artist, album, callback){
 				}
 		}
 		
-		fs.exists('cover/'+path.basename(cover), function( exists ) {
+		fs.exists('/root/app/cover/'+path.basename(cover), function( exists ) {
 			if(!exists && working != url){
 				if(working != url){
 					console.log('Handling new blurring request');
 				}
 				working = url;
-				download(cover, 'cache/'+path.basename(cover));
+				download(cover, '/root/app/cache/'+path.basename(cover));
 				setTimeout(function(){
 					
 					// Sample image
-					gm('cache/'+path.basename(cover)).sample('1x1').write('sample/'+path.basename(cover), function(err){
+					gm('/root/app/cache/'+path.basename(cover)).sample('1x1').write('/root/app/sample/'+path.basename(cover), function(err){
 						if (!err) {
 							console.log('Completed sample request for '+artist+' '+album);
 						}else{
@@ -180,7 +277,7 @@ var coverart = function(artist, album, callback){
 					});
 					
 					// Blur image
-					gm('cache/'+path.basename(cover)).blur(7,3).write('cover/'+path.basename(cover), function(err){
+					gm('/root/app/cache/'+path.basename(cover)).blur(7,3).write('/root/app/cover/'+path.basename(cover), function(err){
 						if (!err) {
 							console.log('Completed blur request for '+artist+' '+album);
 							out = {'success': true, 'file': 'cover/'+path.basename(cover), 'sample': 'sample/'+path.basename(cover)};
@@ -214,18 +311,15 @@ var coverart = function(artist, album, callback){
 
 }
 
-
 // WEBSOCKET AUTH /////////////////////////////////
-io.of('/system').authorization(function (handshakeData, callback) {
-	console.log("HSD", handshakeData);
-	io.sockets.emit('log', {level: 'log', message: "Incoming auth request", aspect: 'systemio'});
+io.of('/system').authorization(function(handshakeData, callback){
 	if(_auth[handshakeData.query.authid] != undefined){
-		io.sockets.emit('log', {level: 'log', message: "Handling auth request for user "+handshakeData.query.authid, aspect: 'mopidy'});
+		serverlog('log', "Handling auth request for user "+handshakeData.query.authid, 'mopidy');
 		callback(null, (_auth[handshakeData.query.authid].key == handshakeData.query.authkey));
 	}else{
 		callback(null, false);
 	}
-}).on('connection', function (socket) {
+}).on('connection', function(socket){
   socket.in('system').emit('ident', {});
   
   socket.on("command", function(data){
@@ -237,23 +331,23 @@ io.of('/system').authorization(function (handshakeData, callback) {
 			 case "playback":
 			 	switch(cmdx[1]){
 					case "play":
-						io.sockets.emit('log', {level: 'log', message: "playback:play has been issued", aspect: 'mopidy'});
+						serverlog('log', "playback:play has been issued", 'mopidy');
 						mopidy.playback.play();
 						break;
 					case "pause":
-						io.sockets.emit('log', {level: 'log', message: "playback:pause has been issued", aspect: 'mopidy'});
+						serverlog('log', "playback:pause has been issued", 'mopidy');
 						mopidy.playback.pause();
 						break;
 					case "stop":
-						io.sockets.emit('log', {level: 'log', message: "playback:stop has been issued", aspect: 'mopidy'});
+						serverlog('log', "playback:stop has been issued", 'mopidy');
 						mopidy.playback.stop();
 						break;
 					case "prev":
-						io.sockets.emit('log', {level: 'log', message: "playback:prev has been issued", aspect: 'mopidy'});
+						serverlog('log', "playback:prev has been issued", 'mopidy');
 						mopidy.playback.previous();
 						break;
 					case "next":
-						io.sockets.emit('log', {level: 'log', message: "playback:next has been issued", aspect: 'mopidy'});
+						serverlog('log', "playback:next has been issued", 'mopidy');
 						mopidy.playback.next();
 						break;
 				}
@@ -261,54 +355,79 @@ io.of('/system').authorization(function (handshakeData, callback) {
 			 case "system":
 			 	switch(cmdx[1]){
 					case "clear":
-						_queue = {};
-						io.sockets.emit('log', {level: 'log', message: "Clearing tracklist and queue", aspect: 'mopidy'});
-						mopidy.tracklist.clear();
-						var tl = returnTracklist();
-						io.sockets.emit('list', {tracks: tl[0], votes: tl[1]});
+						_queue = {}; // TODO: NoSQL
+						
+						nosql.clear(function(){
+							serverlog('log', "Clearing tracklist and queue", 'mopidy');
+							mopidy.tracklist.clear();
+							var tl = returnTracklist();
+							io.sockets.emit('list', {tracks: tl[0], votes: tl[1]});
+						});
+						break;
+					case "populate":
+						break;
+					case "reset":
 						break;
 				}
 			 	break;
 			 case "volume":
-			 	mopidy.playback.setVolume(parseInt(data.level));
+			 	mopidy.playback.setVolume(parseInt(data.data.level));
+			 	break;
+			 case "settings":
+			 	switch(args[1]){
+				 	case "allow":
+				 		break;
+				 	case "queue":
+				 		break;
+				 	case "mode":
+				 		break;
+			 	}
+			 	break;
+			 case "help":
 			 	break;
 			 default:
-			 	io.sockets.emit('log', {level: 'error', message: "Unhandled system command", aspect: 'systemio'});
+			 	serverlog('error', "Unhandled system command", 'systemio');
 		 }
 	 } 
   });
 });
 
 // WEBSOCKET //////////////////////////////////////
-io.on('connection', function (socket) {
-
+io.on('connection', function(socket){
+	serverlog('log', "New client connected", 'systemio');
+	
+	// Ident
+	id = (socket.handshake.sessionid != 'undefined') ? socket.handshake.sessionid : socket.id;
+	socket.emit('ident', {sessionid: id});
+	
+	// Fill
 	var tl = returnTracklist();
 	socket.emit('list', {tracks: tl[0], votes: tl[1]});
 	
-	socket.on('update', function (data) {
+	socket.on('update', function(data){
     console.log("UPDATE");
     var tl = returnTracklist();
     socket.emit('list', {tracks: tl[0], votes: tl[1]});
   });
-	socket.on('vote', function (data) {
+	socket.on('vote', function(data){
 		console.log("VOTE");
-		updateQueue(data.track, 1, socket.id);
+		updateQueue(data.track, 1, socket.handshake.sessionid);
 	});
-	socket.on('queue', function (data) {
+	socket.on('queue', function(data){
 		console.log("QUEUE");
-		updateQueue(data.track, 1, socket.id);
+		updateQueue(data.track, 1, socket.handshake.sessionid);
 	});
-	socket.on('system', function (data) {
+	socket.on('system', function(data){
 		console.log("SYSTEM", data.command);
 		switch(data.command){
 			case 'clear':
-				_queue = {};
+				_queue = {}; // TODO: NoSQL
 				var tl = returnTracklist();
 				socket.emit('list', {tracks: tl[0], votes: tl[1]});
 				break;
 		}
 	});
-	socket.on('coverart', function (data) {
+	socket.on('coverart', function(data){
 		coverart(data.artist, data.album, function(result){
 			io.sockets.emit('cover', result);
 		});
@@ -316,7 +435,7 @@ io.on('connection', function (socket) {
 });
 
 mopidy.on('event:trackPlaybackEnded', function(data){
-	io.sockets.emit('log', {level: 'log', message: 'Playback ended', aspect: 'mopidy'});
+	serverlog('log', 'Playback ended', 'mopidy');
 	console.log("Deleting track "+data.tl_track.track.name);
 	
 	mopidy.tracklist.getTracks().then(function(tracks){
@@ -328,10 +447,11 @@ mopidy.on('event:trackPlaybackEnded', function(data){
 	mopidy.tracklist.remove({tlid: data.tl_track.track.tlid, uri: data.tl_track.track.uri}).then(function(tracks){
 		console.log("REMOVED ", tracks);
 	});
-	io.sockets.emit('log', {level: 'log', message: 'Removed '+data.tl_track.track.name, aspect: 'mopidy'});
+	serverlog('log', 'Removed '+data.tl_track.track.name, 'mopidy');
 	//console.log("PREDEL", _queue);
-	delete _queue[data.tl_track.track.uri];
+	delete _queue[data.tl_track.track.uri]; // TODO: NoSQL
 	//console.log("AFTDEL", _queue);
+	nosql.stored.execute('queueRemove', {key: data.tl_track.track.uri});
 	
 	mopidy.tracklist.getTracks().then(function(tracks){
 		for(var i=0;i<tracks.length;i++){
@@ -342,9 +462,8 @@ mopidy.on('event:trackPlaybackEnded', function(data){
 	var tl = returnTracklist();
 	io.sockets.emit('list', {tracks: tl[0], votes: tl[1]});
 });
-	
 mopidy.on('event:playbackStateChanged', function(data){
-	io.sockets.emit('log', {level: 'log', message: 'fetching cover art', aspect: 'mopidy'});
+	serverlog('log', 'fetching cover art', 'mopidy');
 	mopidy.playback.getCurrentTrack().then(function(track){
 		if(track){
 			console.log("Got track, finding cover art");
@@ -359,7 +478,7 @@ coverart(track.artist[0].name, track.album.name, function(result){
 });
 
 // HTTP ///////////////////////////////////////////
-app.use (function(req, res, next) {
+app.use(function(req, res, next){
 	res.header("Access-Control-Allow-Origin", "*");
     var data = '';
     req.setEncoding('utf8');
